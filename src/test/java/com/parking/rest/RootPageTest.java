3f9e01116eb7d5816d6870ac608b163467f529ca
Antonio Signore <antonio.signore@gmail.com>
2015-02-01 18:57:29 +0100

    Home document

---
+++ src/test/java/com/parking/rest/RootPageTest.java
@@ -0,0 +1,35 @@
+package com.parking.rest;
+
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.test.framework.AppDescriptor;
+import com.sun.jersey.test.framework.WebAppDescriptor;
+import org.codehaus.jettison.json.JSONException;
+import org.junit.Test;
+
+import java.net.URISyntaxException;
+
+public class RootPageTest extends ApplicationTest {
+
+
+
+    @Override
+    protected AppDescriptor configure() {
+        return new WebAppDescriptor.Builder().build();
+    }
+
+    @Test
+    public void testGetRoot() throws JSONException, URISyntaxException {
+
+        String authToken = getToken("admin", "admin");
+
+        WebResource webResource = client().resource("http://localhost:8080/parking/rest");
+        String s = webResource.path("/")
+                .header("X-Auth-Token", authToken)
+                .accept("application/json")
+                .type("application/json")
+                .get(String.class);
+
+        System.out.println("GET root = " + s);
+
+    }
+}
